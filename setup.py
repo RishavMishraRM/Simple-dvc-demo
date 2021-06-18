7c97b292954b81227ff66a56c26f356c79e90967
Rishav Kumar Mishra <rishav.mishra.rkm@gmail.com>
2021-06-19 01:54:09 +0530

    webapps structure added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    description="its a wine Q package", 
-    author="RishavMishraRM", 
+    description="its a wine Q package",
+    author="RishavMishraRM",
     packages=find_packages(),
     license="MIT"
 )
